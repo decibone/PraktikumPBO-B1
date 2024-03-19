cfdb9d11cbe2c9ee1a7ddc738454e6e3cd77938c
Lucky <decibone@gmail.com>
2024-03-19 11:07:13 +0700

    Praktikum 4 - Inheritance

--- Praktikum 4/Inheritance/org/main/MPoligon.java
+++ Praktikum 4/Inheritance/org/main/MPoligon.java
@@ -5,7 +5,10 @@ import org.bangundatar.*;
 public class MPoligon{
     public static void main(String[] args){
         persegiPanjang persegi = new persegiPanjang(10, 10, 4);
+        Segitiga segitiga1 = new Segitiga(5, 10, 3)
         persegi.printInfo();
+        segitiga1.printInfo();
+        System.out.println("Luas segitiga: "+segitiga1.hitungLuas());
         System.out.println("Luas persegi panjang: "+persegi.hitungLuas());
     }
 }
